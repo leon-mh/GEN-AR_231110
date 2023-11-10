e40f4171d43e3177e639fc6065241f535c111a4d
leon-mh <131349737+leon-mh@users.noreply.github.com>
2023-11-10 19:26:27 +0100

    try2

--- js/sketch.js
+++ js/sketch.js
@@ -9,10 +9,10 @@ function setup() {
 function draw() {
 	let x = noise(frameCount * .007) * width
 	let y = noise(frameCount * .005) * height
-	let fc = h + sin(frameCount * .1) * 40
+	// let fc = h + sin(frameCount * .1) * 40
 
-	let c = get(width/2,height/2,100,100)
-	fill(frameCount%255, c, c)
+	// let c = get(width/2,height/2,100,100)
+	fill(frameCount%255, 50, 50)
 	stroke(255)
 	
 	translate(x, y)
