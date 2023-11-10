0c1fe04740cf9d701082c317b28305bfbb5d1462
leon-mh <131349737+leon-mh@users.noreply.github.com>
2023-11-10 19:28:37 +0100

    Try

--- js/sketch.js
+++ js/sketch.js
@@ -13,12 +13,12 @@ function draw() {
 
 	// let c = get(width/2,height/2,100,100)
 	fill(frameCount%255, 50, 50)
-	stroke(255)
+	// stroke(255)
 	
 	translate(x, y)
 	rotate(radians(frameCount * 1.1))
 	
-	rect(0, 0, 300, 3)
+	rect(0, 0, 300, 30)
 	// circle(0,0,300,300)
 }
 
