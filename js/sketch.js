5e88357c5d1f4c5a3aac30d6707cefd0844e2745
leon-mh <131349737+leon-mh@users.noreply.github.com>
2023-11-10 18:51:18 +0100

    First commit

---
+++ js/sketch.js
@@ -0,0 +1,27 @@
+let h = 0
+
+function setup() {
+	createCanvas(895, 1280, document.getElementById('canvas-ar')) // poster aspect
+	pixelDensity(1) // prevent 200+ PPI lag
+	colorMode(HSL)
+}
+
+function draw() {
+	let x = noise(frameCount * .007) * width
+	let y = noise(frameCount * .005) * height
+	let fc = h + sin(frameCount * .1) * 40
+
+	let c = get(width/2,height/2,1,1)
+	fill(c, 50, 50)
+	noStroke()
+
+	translate(x, y)
+	rotate(radians(frameCount * 1.1))
+	// rect(0, 0, 300, 3)
+	circle(0,0,300,300)
+}
+
+function mousePressed() {
+	clear()
+	h = random(360)
+}
